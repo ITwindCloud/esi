b6ea9d81adf10b6e5238c11ea3d78d62c04d2f0b
ITwindCloud <2793567995@qq.com>
2025-05-09 16:58:32 +0800

    add pipeline

--- simu_source_activity.m
+++ simu_source_activity.m
@@ -1,14 +1,4 @@
 addpath('toolbox');
-
-% global parameters
-sample_rate = 1200; %sample rat
-sample_time = 0.5; % sample time
-sp = sample_rate * 0.5;   %sample points
-source_num = 3; % number of source
-
-efficient_win_len = sp ;
-
-%% import previously computed data
 load data/msp_nuts.mat
 load data/nuts_Lp.mat
 nuts.Lp = Lp;
@@ -21,28 +11,11 @@ channel_num  = size(leadfield,1); % number of channel
 for i=1:source_total_num
     leadfield(:,i) = leadfield(:,i) ./norm(leadfield(:,i));
 end
-%% source positions
-source_indexes = define_source_pos(voxels,source_num,25);
 
-% ERP
-tt = sample_time / sp * (1:sp);
-
-% source signals
-low_freq = 1;
-high_freq = 45;
-alp = 0.5;
-phase = 1;
-
-acitivities = source_activity_simu(source_num,sp,tt, alp, low_freq,high_freq,phase,efficient_win_len);
-
-true_source = zeros(source_total_num, sp);
-for i=1:source_num
-    true_source(source_indexes(i),:) = acitivities(i,:);
-end
+[simulated,seed,clean] = get_sources(3,leadfield,voxels);
 
 figure;
-subplot(211);plot(acitivities');
-subplot(212);plot(sum(true_source.^2,2));
-
-%% save data related to true sources
-save(['global-',num2str(source_num),'sources.mat'],"source_num","channel_num","efficient_win_len","voxels","leadfield","source_indexes","acitivities","true_source","noise_rest","sp",'data_type');
+% source activity
+subplot(211);plot(seed');
+% scalp recording
+subplot(212);plot(clean');
